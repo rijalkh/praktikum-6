590df197ec31ed12e07bcbbc3c43635b0d467d30
Khalid Rijaluddin <89462538+rijalkh@users.noreply.github.com>
2022-04-27 09:20:18 +0700

    Create biodata.php

---
+++ soal3/biodata.php
@@ -0,0 +1,39 @@
+<html>
+    <body>
+        <form action="submitBio.php" method="POST">
+        <h2>FORM BIODATA</h2>
+            <table>
+                <tr>
+                    <td>Nama</td>
+                    <td><input type="text" name="nama"></td>
+                </tr>
+
+                <tr>
+                    <td>TTL</td>
+                    <td><input type="text" name="ttl"></td>
+                </tr>
+
+                <tr>
+                    <td>Jenis Kelamin</td>
+                    <td><input type="text" name="kelamin"></td>
+                </tr>
+
+                <tr>
+                    <td>No.Handpone</td>
+                    <td><input type="text" name="hp"></td>
+                </tr>
+
+                <tr>
+                    <td>Alamat</td>
+                    <td><input type="text" name="alamat"></td>
+                </tr>
+
+                <tr>
+                    <td><input type="submit" name="submit" value="Kirim"s></td>
+                    <td><input type="reset" name="reset" value="Reset"s></td>
+                </tr>
+                
+            </table>
+        </form>
+    </body>
+</html>
